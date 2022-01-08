3d1f65ca0e2e0b180cd70d936d2ff147dc2c15b6
BrightBolt <81591421+BrightBolt@users.noreply.github.com>
2022-01-08 02:57:29 -0500

    added loretoken

--- HenryMod/Modules/StaticValues.cs
+++ HenryMod/Modules/StaticValues.cs
@@ -21,7 +21,7 @@ namespace HenryMod.Modules
 
         internal static string characterLore = "How long has it been? He pondered, and quickly decided it did not matter. Time had become irrelevant; all he knew was his duty. To find. To capture. To fight. To kill.\n\n"
             + "Electrocute.\n\n"
-            + "An orphan, they said. Saved from a life spent in darkened alleyways, the only caveat being that he work for them without question. They called themselves scientists. He called them slavers. For any disobedience was swiftly punished with high-voltage pain.\n\n"
+            + "An orphan, they said. Saved from a life spent in darkened alleyways, the only caveat being that he work for them without question. They called themselves scientists. He called them slavers. For any disobedience was swiftly punished with high-voltage retaliation.\n\n"
             + "Electrocute.\n\n"
             + "A new mission, they told him. Obtain an artifact from a desolate planet. The details were left out; if he died, they would simply use another. But when he touched the artifact, sparks flew. Images of a faraway planet, lush and teeming with life, flashed into his mind. He stumbled back, confused. What did this mean? What was this artifact? He knew if he followed the message, he’d find the answers. But first, there was something he must do. For he saw the planet, but also the truth. \n\n"
             + "He was an orphan, yes. But an artificial one.\n\n"
@@ -29,7 +29,7 @@ namespace HenryMod.Modules
             + "The trail back to headquarters was stained with blood. He didn't have to go back. With his power, he could've escaped and never returned. But he wanted vengeance.\n\n"
             + "Electrocute.\n\n"
             + "A trail of charred corpses behind him, he breaks into the boardroom. Desperate businessmen fire their sidearms, but the bullets refuse to hit their target. The last thing they see is a terrifying, magnificent blue light beginning to emanate from their slave-turned-assassin.\n\n"
-            + "Electrocute";
+            + "Electrocute.";
 
         internal static float boltBlastDamageCoefficient = .2f;
         
